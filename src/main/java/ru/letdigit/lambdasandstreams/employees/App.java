b166666fbec8c542c160da73009d89f2fd163de2
Eugene Elantsev <blinktreeman@gmail.com>
2023-11-25 17:49:16 +0900

    add task 2.4

--- src/main/java/ru/letdigit/lambdasandstreams/employees/App.java
+++ src/main/java/ru/letdigit/lambdasandstreams/employees/App.java
@@ -1,6 +1,8 @@
 package ru.letdigit.lambdasandstreams.employees;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 2. Создать класс Employee (Сотрудник) с полями: String name, int age, double salary, String department
@@ -35,6 +37,14 @@ public class App {
         System.out.println("Employees salary up:");
         salaryUpList.forEach(System.out::println);
 
+        // 2.4 * Из списка сотрудников с помощью стрима создать Map<String, List<Employee>>
+        // с отделами и сотрудниками внутри отдела
+        Map<String, List<Employee>> employeesByDepartment = Company.getEmployees()
+                .stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.toList()));
+        System.out.println("Employees by departments: ");
+        employeesByDepartment.entrySet().forEach(System.out::println);
+
     }
 }
 
@@ -49,4 +59,20 @@ Employees salary up:
 Employee{name='Ivanov', age=30, salary=6000.0, department='sales'}
 Employee{name='Petrov', age=50, salary=9600.0, department='administration'}
 Employee{name='Sidorov', age=20, salary=8400.0, department='administration'}
+
+Employees by departments:
+administration=[
+Employee{name='Petrov', age=50, salary=9600.0, department='administration'},
+Employee{name='Sidorov', age=20, salary=8400.0, department='administration'},
+Employee{name='Ivanov', age=25, salary=16000.0, department='administration'},
+Employee{name='Petrov', age=27, salary=17000.0, department='administration'},
+Employee{name='Sidorov', age=29, salary=18000.0, department='administration'},
+Employee{name='Ivanov', age=43, salary=19000.0, department='administration'}
+]
+sales=[
+Employee{name='Ivanov', age=30, salary=6000.0, department='sales'},
+Employee{name='Petrov', age=35, salary=11000.0, department='sales'},
+Employee{name='Sidorov', age=40, salary=12000.0, department='sales'},
+Employee{name='Ivanov', age=45, salary=13000.0, department='sales'}
+]
  */
